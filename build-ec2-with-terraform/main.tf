f1a4c25f01c9600adb2b5b16c1a3a650915214d3
phylynxbelt <thanaporn.jira@odds.team>
2021-07-17 02:39:04 +0700

    feat: add additional terraform with ec2 config

--- build-ec2-with-terraform/main.tf
+++ build-ec2-with-terraform/main.tf
@@ -1,23 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.27"
     }
   }
 
-  required_version = "~> 0.14.9"
+  required_version = "~> 1.0.2"
 }
 
 provider "aws" {
   profile = "default"
-  region = "ap-southeast-1"
+  region  = "ap-southeast-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami = "ami-0d058fe4"
-  instance_type = "t2.micro"
-
+  ami                    = "ami-0d058fe428540cd89"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-083af83e9f0092924"]
+  subnet_id              = "subnet-07a5c87338403f38f"
   tags = {
     "Name" = "app-server-terraform"
   }
